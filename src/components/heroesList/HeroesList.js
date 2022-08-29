a928f48d5fb11a03b9500335d04ceba7111ae2f2
ilija <komperdel@gmail.com>
2022-08-29 16:28:08 +0300

    remove ench&thunk,add toolkit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -3,7 +3,7 @@ import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import { heroDeleted, fetchHeroes } from '../../actions';
 import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
 
@@ -43,10 +43,7 @@ const HeroesList = (props) => {
 	const { request } = useHttp();
 
 	useEffect(() => {
-		dispatch('HEROES_FETCHING');
-		request('http://localhost:3001/heroes')
-			.then((data) => dispatch(heroesFetched(data)))
-			.catch(() => dispatch(heroesFetchingError()));
+		dispatch(fetchHeroes(request));
 
 		// eslint-disable-next-line
 	}, []);
--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,29 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer =
-	(createStore) =>
-	(...args) => {
-		const store = createStore(...args);
+const stringMiddleware = () => (next) => (action) => {
+	if (typeof action === 'string') {
+		return next({
+			type: action,
+		});
+	}
+	return next(action);
+};
 
-		const oldDispatch = store.dispatch;
-		store.dispatch = (action) => {
-			if (typeof action === 'string') {
-				return oldDispatch({
-					type: action,
-				});
-			}
-			return oldDispatch(action);
-		};
-		return store;
-	};
+// const store = createStore(
+// 	combineReducers({ heroes: heroes, filters: filters }),
+// 	compose(
+// 		applyMiddleware(ReduxThunk, stringMiddleware),
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 	)
+// );
 
-const store = createStore(
-	combineReducers({ heroes: heroes, filters: filters }),
-	compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
+const store = configureStore({
+	reducer: { heroes: heroes, filters: filters },
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware),
+	devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
 
